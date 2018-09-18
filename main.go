27fb3810037ea7305632231dc5ea75fc933922fc
Koji Itahana <kouji.itahana@g.softbank.co.jp>
2018-09-18 15:27:30 +0900

    test commit

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 // Connpass is ...
 type Connpass struct {
-  //ResultsReturned  int `json:results_returned`
-  //ResultsAvailable int `json:results_available`
-  //ResultsStart     int `json:results_start`
-  ResultsReturned  int `json:"results_returned"`
-  ResultsAvailable int `json:"results_available"`
-  ResultsStart     int `json:"results_start"`
+  ResultsReturned  int `json:results_returned`
+  ResultsAvailable int `json:results_available`
+  ResultsStart     int `json:results_start`
+  //ResultsReturned  int `json:"results_returned"`
+  //ResultsAvailable int `json:"results_available"`
+  //ResultsStart     int `json:"results_start"`
 }
 
 func main() {
