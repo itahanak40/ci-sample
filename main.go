526eade0547b251d1f8ab8dd97a8e1d976737cbc
Koji Itahana <kouji.itahana@g.softbank.co.jp>
2018-09-18 11:16:59 +0900

    go vet OKパターン

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package ci_sample
 import "fmt"
 
 type Connpass struct {
-  ResultsReturned  int `json:results_returned`
-  ResultsAvailable int `json:results_available`
-  ResultsStart     int `json:results_start`
-  //ResultsReturned  int `json:"results_returned"`
-  //ResultsAvailable int `json:"results_available"`
-  //ResultsStart     int `json:"results_start"`
+  //ResultsReturned  int `json:results_returned`
+  //ResultsAvailable int `json:results_available`
+  //ResultsStart     int `json:results_start`
+  ResultsReturned  int `json:"results_returned"`
+  ResultsAvailable int `json:"results_available"`
+  ResultsStart     int `json:"results_start"`
 }
 
 func main() {
